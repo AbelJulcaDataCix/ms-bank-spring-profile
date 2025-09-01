c6947af93ef17e6b2370f49832ff187f169fe90a
AbelJulcaDataCix <datacixcustomer@gmail.com>
2025-08-31 22:31:36 -0500

    refactor method type customer controller

--- profile/src/main/java/com/dataprogramming/profile/controller/TypeCustomerController.java
+++ profile/src/main/java/com/dataprogramming/profile/controller/TypeCustomerController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -27,8 +28,20 @@ public class TypeCustomerController {
     }
 
     @GetMapping("/find/{id}")
-    public Mono<TypeCustomer> findById(@PathVariable String id){
-        return typeCustomerService.findById(id);
+    public Mono<ResponseEntity<TypeCustomer>> findById(@PathVariable String id){
+
+        if (!StringUtils.hasText(id)) {
+            return Mono.just(new ResponseEntity<>(HttpStatus.BAD_REQUEST));
+        }
+
+        return typeCustomerService.findById(id)
+                .map(subType -> new ResponseEntity<>(subType, HttpStatus.OK))
+                .switchIfEmpty(Mono.just(new ResponseEntity<>(HttpStatus.NOT_FOUND)))
+                .doOnSuccess(response -> log.info("Find operation successful with status: {}", response.getStatusCode()))
+                .onErrorResume(throwable -> {
+                    log.error("Error during find operation:", throwable);
+                    return Mono.just(new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR));
+                });
     }
 
     @PostMapping("/create")
@@ -45,17 +58,27 @@ public class TypeCustomerController {
     }
 
     @PutMapping("/update")
-    public Mono<ResponseEntity<TypeCustomer>> update(@Valid @RequestBody TypeCustomer c) {
-        return typeCustomerService.update(c)
+    public Mono<ResponseEntity<TypeCustomer>> update(@Valid @RequestBody TypeCustomer typeCustomer) {
+        return typeCustomerService.update(typeCustomer)
                 .map(savedCustomer -> new ResponseEntity<>(savedCustomer, HttpStatus.CREATED))
                 .defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @DeleteMapping("/delete/{id}")
-    public Mono<ResponseEntity<String>> delete(@PathVariable String id) {
+    public Mono<ResponseEntity<Void>> delete(@PathVariable String id) {
         return typeCustomerService.delete(id)
-                .filter(deleteCustomer -> deleteCustomer)
-                .map(deleteCustomer -> new ResponseEntity<>("Customer Deleted", HttpStatus.ACCEPTED))
-                .defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+                .flatMap(isDeleted -> {
+                    if (isDeleted) {
+                        return Mono.just(new ResponseEntity<Void>(HttpStatus.NO_CONTENT));
+                    } else {
+                        log.warn("not find object for delete");
+                        return Mono.just(new ResponseEntity<Void>(HttpStatus.NOT_FOUND));
+                    }
+                })
+                .doOnSuccess(responseEntity -> log.info("subType delete completed with status: {}", responseEntity.getStatusCode()))
+                .onErrorResume(throwable -> {
+                    log.error("Error during delete operation: ", throwable);
+                    return Mono.just(new ResponseEntity<Void>(HttpStatus.INTERNAL_SERVER_ERROR));
+                });
     }
 }
